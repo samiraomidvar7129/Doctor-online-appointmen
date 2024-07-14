28933e403ede19b293066ec4fa05128cb1c54451
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-14 02:35:22 -0700

    add new styles

---
+++ Assets/Js/swipers.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 600,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+    },
+  });
