18417451dc9969626f7077bee75526199fbac7c4
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-16 07:21:50 -0700

    create ASE_URL variable

--- Assets/Js/doctorsFetch.js
+++ Assets/Js/doctorsFetch.js
@@ -62,11 +62,14 @@ document.addEventListener("DOMContentLoaded", function () {
   var xhttp = new XMLHttpRequest();
 
   //step 2:
-
-  var url = "/Assets/json/doctorsList.json";
-  xhttp.open("GET", url);
+  const BASE_URL="/Assets/json"
 
   //step 3:
+
+  var url = `${BASE_URL}/doctorsList.json`;
+  xhttp.open("GET", url);
+
+  //step 4:
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var data = JSON.parse(xhttp.responseText);
