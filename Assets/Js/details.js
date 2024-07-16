e397a7985d120388491be9610e85bc32bb2be9e3
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-16 09:35:06 -0700

    add BASE_URL

--- Assets/Js/details.js
+++ Assets/Js/details.js
@@ -4,12 +4,15 @@ document.addEventListener("DOMContentLoaded", function () {
   //step 1:
   var xhttp = new XMLHttpRequest();
 
-  //step 2:
+   //step 2:
+  const BASE_URL="/Assets/json"
    
-  var url="/Assets/json/ghalb.json";
+   //step 3:
+
+   var url = `${BASE_URL}/doctorsList.json`;
   xhttp.open("GET", url);
 
-  //step 3:
+  //step 4:
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       var data =JSON.parse(xhttp.responseText);
