dad0a017f6ca898da9c8df2a675e9ae55d167fd8
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-16 23:48:39 -0700

    create displayDoctors func

---
+++ Assets/Js/scripts/displayData.js
@@ -0,0 +1,30 @@
+export const displayDoctors = (doctors) => {
+  const doctorsContainer = document.querySelector(".mySwiper");
+
+  doctorsContainer.innerHTML = doctors.map((doctor) => {
+    const doctorsList = document.querySelector(".swiper-wrapper");
+
+    doctorsList.innerHTML += `
+    <div class="swiper-slide bg-light d-flex justify-content-center align-items-center text-center">
+          <a href=${"details.html?group=" + doctor.id} 
+          class="mySwiper-list_item p-3 d-flex flex-column justify-content-center align-items-center w-100 h-100 ">
+            <div class="class="mySwiper-list_item--imgBox d-flex justify-content-center align-items-center w-100 h-100">
+                <img src=${
+                  doctor.ImageUrl
+                } class="doctor-ImageUrl rounded-pill d-block w-100 h-100 cover  alt="doctor-img">
+            </div>
+            <div class="doctor-name pt-3">${doctor.Name} </div>
+            <div class=" doctor-Speciallity pt-3">${doctor.Speciallity}</div>
+            <div class="d-flex justify-content-between align-items-center w-100">
+                         <div class=" doctor-NezamCode pt-3">    ن-پ :  ${
+                           doctor.NezamCode
+                         } </div>
+                         <div class=" doctor-city pt-3">  ${doctor.city} </div>
+ 
+            </div>
+                 
+            </a>
+       </div>      
+   `;
+  });
+};
