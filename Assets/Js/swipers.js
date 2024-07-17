f843587f001f1c218b56eb06d807d3e50834a23d
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-17 01:59:24 -0700

    chnage comments slider

--- Assets/Js/swipers.js
+++ Assets/Js/swipers.js
@@ -25,3 +25,59 @@ var swiper = new Swiper(".mySwiper", {
       },
     },
   });
+
+  var swiper = new Swiper("#comments", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 900,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+  });
+
+  var swiper = new Swiper("#blogs", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+  });
