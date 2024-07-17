d020aee73b053aba432d708ff6f5fb05c222bfc1
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-17 02:01:53 -0700

    edit displayBlogs

--- Assets/Js/scripts/displayData.js
+++ Assets/Js/scripts/displayData.js
@@ -1,9 +1,8 @@
-
-
 export const displayDoctors = (doctors) => {
   const doctorsContainer = document.querySelector(".swiper-wrapper");
-  for(let doctor in doctors){
-doctorsContainer.innerHTML = doctors[doctor].map(item=>`
+  for (let doctor in doctors) {
+    doctorsContainer.innerHTML = doctors[doctor].map(
+      (item) => `
   
     <div class="swiper-slide bg-light d-flex justify-content-center align-items-center text-center">
           <a href=${"details.html?group=" + item.id} 
@@ -17,7 +16,7 @@ doctorsContainer.innerHTML = doctors[doctor].map(item=>`
             <div class=" doctor-Speciallity pt-3">${item.Speciallity}</div>
             <div class="d-flex justify-content-between align-items-center w-100">
                          <div class=" doctor-NezamCode pt-3">    ن-پ :  ${
-                          item.NezamCode
+                           item.NezamCode
                          } </div>
                          <div class=" doctor-city pt-3">  ${item.city} </div>
  
@@ -26,19 +25,17 @@ doctorsContainer.innerHTML = doctors[doctor].map(item=>`
             </a>
        </div>      
    
-  `)
-}
-  
-
-  
+  `
+    );
+  }
 };
 
 export const displayComments = (comments) => {
   const commentsContainer = document.querySelector("#comments");
 
-
-  for(let comment in comments){
-    commentsContainer.innerHTML = comments[comment].map(item=>`
+  for (let comment in comments) {
+    commentsContainer.innerHTML = comments[comment].map(
+      (item) => `
     <div class="swiper-slide bg-info p-3 d-flex flex-column justify-content-center align-items-center text-center">
          <div>
          <img src=${item.userImg} alt="user-img"/>
@@ -46,26 +43,24 @@ export const displayComments = (comments) => {
          <div><p>${item.userComment}</p></div>
          <div>${item.userName}</div>
        </div>      
-   `)
-    }
-
-
- 
+   `
+    );
+  }
 };
 
 export const displayBlogs = (blogs) => {
   const blogsContainer = document.querySelector("#blogs");
 
-
-  for(let blog in blogs){
-    blogsContainer.innerHTML = blogs[blog].map(item=>`
+  for (let blog in blogs) {
+    blogsContainer.innerHTML = blogs[blog].map(
+      (item) => `
     <div class="swiper-slide bg-info  p-3 d-flex flex-column justify-content-center align-items-center text-center">
          <div>
          <img src=${item.img} class="d-block w-100 h-100 cover" alt="user-img"/>
          </div>
          <div><p>${item.desc}</p></div>
        </div>      
-   `)
-    }
-
+   `
+    );
+  }
 };
