6d33da8edc70c22db0560533ffd7d94905502849
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-16 23:57:41 -0700

    import displayDoctors

--- Assets/Js/scripts/main.js
+++ Assets/Js/scripts/main.js
@@ -1,8 +1,14 @@
 import{fetchDoctorsList} from './fetchDoctors';
+import {fetchComments} from './fetchComments';
+import {fetchBlogs} from './fetchBlogs';
+import {displayDoctors} from './displayData';
+
 
 document.addEventListener('DOMContentLoaded',()=>{
     Promise.all([
-        fetchDoctorsList()
+        fetchDoctorsList(),
+        fetchComments(),
+        fetchBlogs()
 
         .then(([doctors])=>{
             displayDoctors(doctors)
