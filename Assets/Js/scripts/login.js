38b7b4570af136ba9de06a6d1496853c86f0aa1e
samiraomidvar7129 <93695009+samiraomidvar7129@users.noreply.github.com>
2024-07-24 08:08:46 -0700

    import validateLogin function

--- Assets/Js/scripts/login.js
+++ Assets/Js/scripts/login.js
@@ -3,6 +3,17 @@ import { validateLogin } from "./validate.js";
 document.getElementById().addEventListener('submit',(event)=>{
     event.preventDefault();
 
+const fullName=document.getElementById('login-fullName').ariaValueMax.trim();
+const password=document.getElementById('login-password').ariaValueMax.trim();
 
+
+
+if(validateLogin(fullName,password)){
+swal.fire('با موفقیت وارد شدید').then(()=>{
+    window.location.href='index.html'
+})
+}else{
+    swal.fire('شماره تماس یا رمز عبور اشتباه است')
+}
 
 })
